16c7eab98ae3d738ec9177a471c32daeca2e9a07
Mauro Piva <piva.1532581@studenti.uniroma1.it>
2019-07-03 10:28:50 +0200

    update compatibility with solidity 0.5.0

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ The token for Kiku, an Andy Wharol Art Work. www.artsquare.io
 
 Next steps: 
 1) ERC20 compliance
-2) make haltable
+2) make haltable (with pausable from ledzeppelin)
 3) insert tests
--- WRL01.sol
+++ WRL01.sol
@@ -1,14 +1,14 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.5.0;
 
-import "zeppelin-solidity/contracts/ownership/Ownable.sol";
-import "zeppelin-solidity/contracts/token/StandardToken.sol";
+import "github.com/OpenZeppelin/zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "github.com/OpenZeppelin/zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 /**
  * @title WRL01 Token
  *
  * @dev Ownable ERC20 token, v 0.1
  */
-contract WRL01 is Ownable {
+contract WRL01 is ERC20, Ownable {
 
   string public constant name = "Kiku - Andy Wharol";
   string public constant symbol = "WRL01";
@@ -29,13 +29,17 @@ contract WRL01 is Ownable {
     _;
   }
 
+  /** The function can be called only before or after the tokens have been releasesd */
+  modifier inReleaseState(bool releaseState) {
+    require(releaseState == released);
+    _;
+  }
 
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
-  function WRL01() {
-    totalSupply = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
+  constructor() public {
+    _mint(msg.sender, INITIAL_SUPPLY);
   }
 
 
@@ -44,15 +48,15 @@ contract WRL01 is Ownable {
   }
 
 
-  function approve(address _to, uint _value) returns (bool ok) {
+  function approve(address _to, uint _value) public returns (bool ok) {
     return super.approve(_to, _value);
   }
 
-  function transfer(address _to, uint _value) canTransfer(msg.sender) returns (bool success) {
+  function transfer(address _to, uint _value) public canTransfer(msg.sender) returns (bool success) {
     return super.transfer(_to, _value);
   }
 
-  function transferFrom(address _from, address _to, uint _value) canTransfer(_from) returns (bool success) {
+  function transferFrom(address _from, address _to, uint _value) public canTransfer(_from) returns (bool success) {
     return super.transferFrom(_from, _to, _value);
   }
 
